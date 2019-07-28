0aba4fc9af33bcee61545228696a43590a695f33
Marina <marinataran27@gmail.com>
2019-07-28 16:54:41 +0300

    designe and token on device

--- app/src/main/java/com/example/marina/myspotifyapp/MyService.java
+++ app/src/main/java/com/example/marina/myspotifyapp/MyService.java
@@ -1,13 +1,13 @@
 package com.example.marina.myspotifyapp;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.support.v4.app.NotificationCompat;
-import android.util.Log;
+        import android.app.Notification;
+        import android.app.NotificationManager;
+        import android.support.v4.app.NotificationCompat;
+        import android.util.Log;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
+        import com.google.firebase.iid.FirebaseInstanceId;
+        import com.google.firebase.messaging.FirebaseMessagingService;
+        import com.google.firebase.messaging.RemoteMessage;
 
 public class MyService extends FirebaseMessagingService {
     String TAG ="MyService";
